472df5aaff94ba5fe9936ce472ee0c893cf88776
LosVetaliy <losvetaliy@gmail.com>
2022-10-11 19:38:02 +0300

    Edit primes description

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,6 +1,6 @@
 import gamePattern from '../index.js';
 
-const description = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
 const prime = (number) => {
   for (let i = 2; i < number; i += 1) {
