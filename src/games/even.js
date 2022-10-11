6f37f26a6d6309acee7ea6911bde5f53b20e760e
LosVetaliy <losvetaliy@gmail.com>
2022-10-11 19:29:18 +0300

    Edit description

--- src/games/even.js
+++ src/games/even.js
@@ -1,7 +1,7 @@
 import gamePattern from '../index.js';
 import getRandomNumber from '../mathRandom.js';
 
-const description = 'Answer "yes" if number even otherwise answer "no".';
+const description = 'Answer "yes" if number even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 const game = () => {
   const maxNumber = 101;
